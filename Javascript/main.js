89e9269c97a34cff726f58079fd6227a24000d5b
Diego Ferreira <dlins_ferreira@hotmail.com>
2023-12-03 12:58:19 -0500

    Comments in the code from the original

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,54 +1,58 @@
+/////////////////////////////////////////////// Login Script ///////////////////////////////////////////////
+
 var quizAnswers = JSON.parse(window.sessionStorage.getItem("quizAnswers")) || {};
 
+// Script for handling login
 $(document).ready(function() {
+    // Event handler for login link on home page
     $('#homePageLoginLink').on('click', function(event) {
         event.preventDefault();
         $('#loginForm').show();
     });
 
+    // Event handler for cancel login on home page
     $('#cancelLoginHomePage').on('click', function() {
         $('#loginForm').hide();
         window.location.href = 'index.html';
     });
 
+    // Event handler for login link on about us page
     $('#aboutUsLoginLink').on('click', function(event) {
         event.preventDefault();
         $('#aboutUsContent').hide();
         $('#aboutUsLoginForm').show();
     });
 
+    // Event handler for cancel login on about us page
     $('#cancelLogin').on('click', function() {
         $('#aboutUsLoginForm').hide();
         $('#aboutUsContent').show();
     });
 
+    // Event handler for logout
     $('#logout').on('click', function() {
         sessionStorage.removeItem('authenticated');
         window.location.href = '../index.html';
     });
 });
 
-
-
-
-
-
-
-
-
-
+// Function for handling login submission
 function handleSubmit(event) {
     event.preventDefault();
     var username = $('#username').val();
     var password = $('#password').val();
 
     if (username === 'user1' && password === 'passAdmin01#') {
-        window.location.href = 'Content/questionnaire.html';
+        window.location.href = 'Content/questionnaire.html'; // Link alterado do 
     } else {
         $('#errorMessage').text('Invalid Username or Password!');
     }
 }
 
+
+/////////////////////////////////////////////// Questionnaire Script ///////////////////////////////////////////////
+
+// Function to validate radio buttons
 function validateRadioSelection() {
     document.querySelectorAll('input[type="radio"]').forEach(function(input) {
         input.addEventListener('change', function() {
@@ -62,10 +66,12 @@ function validateRadioSelection() {
     });
 }
 
-window.onload = function() {
+window.onload = function() 
+{
     validateRadioSelection();
 };
 
+// Function for handling user input for the questionnaire
 function user_input(pageNumber) {
     var selectedOptions = [];
     var opt_a = $('input[name="opt_a"]:checked').val();
@@ -90,6 +96,7 @@ function user_input(pageNumber) {
     }
 }
 
+// Function to advance to the next page of the questionnaire
 function goForward(pageNumber) {
     if (user_input(pageNumber)) {
         var nextPage = pageNumber + 1;
@@ -101,16 +108,25 @@ function goForward(pageNumber) {
     }
 }
 
+
+/////////////////////////////////////////////// Result Script ///////////////////////////////////////////////
+
+// Function to fill up the results table
 function fillUpTable() {
     var somaA = 0;
     var somaB = 0;
     var somaC = 0;
     var somaD = 0;
 
+    // Loop through quizAnswers and calculate the sums
     for (var i = 1; i <= Object.keys(quizAnswers).length; i++) {
+        // Acessando as respostas da página i
         var answersPage = quizAnswers['q' + i];
 
-        if (answersPage) {
+        // Verifying if the answers exist
+        if (answersPage) 
+        {
+            // Associating each answer to a variable
             var answerA = answersPage[0];
             somaA += answerA || 0;
 
@@ -123,6 +139,7 @@ function fillUpTable() {
             var answerD = answersPage[3];
             somaD += answerD || 0;
 
+            // Updating table cells with individual answers
             document.getElementById('varA' + i).textContent = answerA || '-';
             document.getElementById('varB' + i).textContent = answerB || '-';
             document.getElementById('varC' + i).textContent = answerC || '-';
@@ -130,10 +147,12 @@ function fillUpTable() {
         }
     }
 
+    // Fill the total values in the table
     document.getElementById('totalA').textContent = somaA;
     document.getElementById('totalB').textContent = somaB;
     document.getElementById('totalC').textContent = somaC;
     document.getElementById('totalD').textContent = somaD;
+
 
     var pageSums = [
         { page: 'A_blue.html', sum: somaC },
@@ -142,20 +161,24 @@ function fillUpTable() {
         { page: 'A_orange.html', sum: somaA }
     ];
 
+    // Sort the pageSums array in descending order based on sum values
     pageSums.sort(function(a, b) { return b.sum - a.sum; });
 
     var topPages = pageSums.slice(0, 2);
 
+    // Display links to the two pages with the highest sum values
     for (var i = 0; i < topPages.length; i++) {
         var pageName = topPages[i].page;
         var color = pageName.split('_')[1].split('.')[0];
         var linkText = 'Your ' + (i === 0 ? 'first' : 'second') + ' color is ' + color.charAt(0).toUpperCase() + color.slice(1);
         var link = '<a href="' + pageName + '">' + linkText + '</a><br><br>';
 
+        // Append the link to the specified element in result.html
         document.getElementById('topColors').innerHTML += link;
     }
 }
 
+// Call the fillUpTable function when the results page is loaded
 $(document).ready(function() {
     if (window.location.pathname.includes('results.html')) {
         fillUpTable();
